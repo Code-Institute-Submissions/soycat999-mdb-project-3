0b8620464b66d03e7c5a969417c007d288afffd9
Ubuntu <ubuntu@ip-172-31-27-5.us-east-2.compute.internal>
2019-12-27 15:16:40 +0000

    connected app.py file to the Mongo database

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'myMDB'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-8nixy.mongodb.net/myDB?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/get_myMDB')
+def get_myMDB():
+    return render_template("matches.html", myMDB=mongo.db.myMDB.find())
     
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
+        
+# Remember to install python 3! sudo pip3 install dnspython > sudo pip3 install pymongo 
