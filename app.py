7346cb33457337e0f117d8f6b9c29e8bbc7028f7
Ubuntu <ubuntu@ip-172-31-27-5.us-east-2.compute.internal>
2020-01-03 05:06:18 +0000

    created a new route for profile page

--- app.py
+++ app.py
@@ -4,17 +4,23 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'myMDB'
+app.config["MONGO_DBNAME"] = 'myDB'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-8nixy.mongodb.net/myDB?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_myMDB')
-def get_myMDB():
-    return render_template("matches.html", myMDB=mongo.db.myMDB.find())
-    
+@app.route('/get_matches')
+def get_matches():
+    return render_template('matches.html', matches=mongo.db.matches.find())
+
+@app.route('/add_profile', methods=['POST'])
+def add_profile():
+    profile = mongo.db.profile
+    profile.insert_one(request.form.to_dict())
+    return redirect(url_for('account.html'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
